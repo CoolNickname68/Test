e25e0b4aeb5c42cc91134de3cf0eb30c563f40b4
dima.dmitri.vladimirov@mail.ru <dima.dmitri.vladimirov@mail.ru>
2023-12-21 23:41:15 +0300

    update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Привет мир!");
+        let dog = 8.0
+        let cat = 3.6
+        let paper = 763789
+        System.out.println(dog);
+        System.out.println(cat);
     }
 }
